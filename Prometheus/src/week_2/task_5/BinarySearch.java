5d955024b2494ec0426102109512b056c84272e4
Dmytro Gumeniuk <d.gumeniuk.javadev@gmail.com>
2017-03-26 00:11:24 +0200

    Task_5 Initial commit

---
+++ Prometheus/src/week_2/task_5/BinarySearch.java
@@ -0,0 +1,15 @@
+package week_2.task_5;
+
+public class BinarySearch {
+
+	public static void main(String[] args) {
+
+		int data[] = { 3, 6, 7, 10, 34, 56, 60 };
+		int numberToFind = 10;
+
+		// PUT YOUR CODE HERE
+
+		// PUT YOUR CODE HERE
+	}
+}
+      
